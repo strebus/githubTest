97f356197fc6561fd5f07c0c5fffca0b5247542b
Sven Trebus <s.trebus@atenekom.eu>
2019-06-07 10:14:32 +0200

    add another message

--- src/App.java
+++ src/App.java
@@ -4,6 +4,6 @@ public class App {
         System.out.println("Hello World");
         // new
         System.out.println("Nice to meet you");
-        System.out.println("this is a test");
+        System.out.println("this is a test with github");
     }
 }
